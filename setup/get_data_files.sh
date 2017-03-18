84e1ee1169f0d90367837a9e0a1969e1c4581c84
David Woo <david.woo@instacart.com>
2017-03-17 23:02:57 -0700

    notes from class

--- setup/get_data_files.sh
+++ setup/get_data_files.sh
@@ -1,3 +1,17 @@
 wget http://www.platform.ai/data/imagenet-sample-train.tar
 wget http://www.platform.ai/data/trn_resized_288.tar
 wget http://www.platform.ai/data/trn_resized_72.tar
+
+wget http://www.platform.ai/part2/lesson2/imagenet_process.ipynb
+
+#move files to data
+cd ..
+mkdir data
+
+mv setup/imagenet-sample-train.tar data
+mv setup/trn_resized_288.tar data
+mv setup/trn_resized_72.tar data
+
+
+tar -xvf trn_resized_288.tar
+tar -xvf trn_resized_72.tar
